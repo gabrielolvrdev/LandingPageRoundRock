85110b58a8b89e60222acab3c40023e1cb2d39be
Gabriel Oliveira <gogabriel35@gmail.com>
2024-06-16 18:07:38 -0300

    landingPage finalizada

---
+++ RoundRock/src/components/cardComponent/CardComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import './Card.scss'
+</script>
+<template>
+  <div class="contentCard">
+    <div>
+      <img src="../../assets/img/ellipse.svg" />
+    </div>
+    <div class="cardContentWhere">
+      <div>
+        <p class="textCardEllipse">Courtney Henry</p>
+      </div>
+      <div>
+        <p class="textParagraphEllipse">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+          been the industry's standard dummy text ever since the 1500s, when an unknown printer took
+          a galley of type and scrambled it to make a type specimen book
+        </p>
+        <div class="contentStar">
+          <img src="../../assets/img/star.svg" />
+          <img src="../../assets/img/star.svg" />
+          <img src="../../assets/img/star.svg" />
+          <img src="../../assets/img/star.svg" />
+          <img src="../../assets/img/star.svg" />
+          <img src="../../assets/img/star.svg" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- RoundRock/src/views/home/HomeView.vue
+++ RoundRock/src/views/home/HomeView.vue
@@ -1,97 +1,125 @@
 <script setup lang="ts">
 import ButtonComponent from '../../components/ButtonComponent/ButtonComponent.vue'
 import ListComponent from '../../components/ListComponent/ListComponent.vue'
+import CardComponent from '@/components/cardComponent/CardComponent.vue'
 import './Home.scss'
 </script>
 
 <template>
-  <section class="contentPrimary">
-    <div class="contentHome">
-      <div class="titleContent">
-        <img src="../../assets/img/Plumbers.svg" />
-        <div class="textHome">
-          <h2 class="text1">Round Rock</h2>
-          <h4 class="text0">Plumbing</h4>
+  <section class="contentRoundRock">
+    <div class="contentHomeRoundRock">
+      <div class="titleContentRoundRock">
+        <img class="imagePlumbers" src="../../assets/img/Plumbers.svg" />
+        <div class="contentTextHome">
+          <h2 class="textRoundRock">Round Rock</h2>
+          <h4 class="subtitleRoundRock">Plumbing</h4>
         </div>
       </div>
       <div>
-        <p class="text3">What is Lorem Ipsum</p>
+        <p class="textTitleRoundRock">What is Lorem Ipsum</p>
       </div>
-      <div class="contentText2">
-        <p class="text2">
+      <div class="contentParagraphRound">
+        <p class="textParagraphRound">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industry's standard dummy text ever since the 1500s, when an unknown printer took
           a galley of type and scrambled
         </p>
       </div>
-      <div class="contentCards">
-        <div class="cardContent"></div>
-        <div class="cardContent"></div>
-        <div class="cardContent"></div>
+      <div class="contentCardsServices">
+        <div class="cardService">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
+        <div class="cardService">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
+        <div class="cardService">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
       </div>
-      <div class="contentButtonPrimary">
-        <ButtonComponent class="buttonContentPrimary"> Help Me </ButtonComponent>
+      <div class="contentButtonHelpMe">
+        <ButtonComponent class="buttonHelpMe"> Help Me </ButtonComponent>
       </div>
     </div>
   </section>
-  <section class="contentSecondaryService">
-    <div class="contentWhite">
-      <div class="contentWork1">
-        <p class="text1">what is Lorem Ipsum</p>
+  <section class="contentWorkService">
+    <div class="contentCardWhiteWork">
+      <div class="contentWork">
+        <p class="textTitleRoundRock">what is Lorem Ipsum</p>
         <p class="textWork">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industry's standard dummy text ever since the 1500s, when an unknown printer took
           a galley of type and scrambled it to make a type specimen book. It has survived not only
           five centuries
         </p>
-        <ListComponent class="textCheck1"
+        <ListComponent class="textCheck"
           >Lorem Ipsum is simply dummy text of the printing</ListComponent
         >
-        <ListComponent class="textCheck1"
+        <ListComponent class="textCheck"
           >Lorem Ipsum is simply dummy text of the printing</ListComponent
         >
-        <ListComponent class="textCheck1"
+        <ListComponent class="textCheck"
           >Lorem Ipsum is simply dummy text of the printing</ListComponent
         >
       </div>
     </div>
-    <div class="contentPhotoWork">
-      <div class="contentWork">
-        <img src="../../assets/img/person.png" />
+    <div class="contentPersonWork">
+      <div class="contentPerson">
+        <img class="imagePerson" src="../../assets/img/person.png" />
       </div>
     </div>
   </section>
   <section class="sectionService">
     <div class="contentService">
       <p class="serviceText">Services</p>
-      <div class="contentCards">
-        <div class="cardContent"></div>
-        <div class="cardContent"></div>
-        <div class="cardContent"></div>
-        <div class="cardContent"></div>
+      <div class="contentCardsServicesWork">
+        <div class="cardService1">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
+        <div class="cardService1">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
+        <div class="cardService1">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
+        <div class="cardService1">
+          <img class="imageWrench" src="../../assets/img/wrench.svg" />
+        </div>
       </div>
     </div>
-    <div class="serviceText2">
-      <div class="CardTextService">
-        <p class="textLorem">Lorem Ipsum</p>
-        <p class="subTextLorem">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+    <div class="contentServiceText">
+      <div class="cardTextService">
+        <p class="titleService">Lorem Ipsum</p>
+        <p class="subtitleService">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </p>
       </div>
-      <div class="CardTextService">
-        <p class="textLorem">Lorem Ipsum</p>
-        <p class="subTextLorem">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+      <div class="cardTextService">
+        <p class="titleService">Lorem Ipsum</p>
+        <p class="subtitleService">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </p>
       </div>
-      <div class="CardTextService">
-        <p class="textLorem">Lorem Ipsum</p>
-        <p class="subTextLorem">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+      <div class="cardTextService">
+        <p class="titleService">Lorem Ipsum</p>
+        <p class="subtitleService">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </p>
       </div>
-      <div class="CardTextService">
-        <p class="textLorem">Lorem Ipsum</p>
-        <p class="subTextLorem">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+      <div class="cardTextService">
+        <p class="titleService">Lorem Ipsum</p>
+        <p class="subtitleService">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+        </p>
       </div>
     </div>
-    <ButtonComponent class="buttonContentPrimary"> Help Me</ButtonComponent>
+    <ButtonComponent class="buttonHelpMe"> Help Me</ButtonComponent>
   </section>
-  <section class="testymonials">
-    <div>teste</div>
+  <section class="sectionTestymonials">
+    <div class="contentCardsTestymonials">
+      <CardComponent></CardComponent>
+      <CardComponent></CardComponent>
+      <CardComponent></CardComponent>
+      <CardComponent></CardComponent>
+    </div>
   </section>
 </template>
